f5aac936ee99bf04303e5598ebbc83a88ec52947
Mridul Chopra <mridulchopra444@gmail.com>
2021-07-02 00:25:20 +0530

    technical skills tab added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+$(document).ready(() => {
+    $('.technical-skills').hide();
+})
+
 
 $(".menu-icon").click(function () {
 
@@ -8,3 +12,25 @@ $(".menu-icon").click(function () {
     else
         $(".nav-items-container").animate({ opacity: 10 }, 2000);
 });
+
+
+$('#about-me').click(function () {
+    change_colour('#about-me');
+    $('.about-me').show();
+    $('.technical-skills').hide();
+})
+
+$('#technical-skills').click(function () {
+    change_colour('#technical-skills');
+    $('.technical-skills').show();
+    $('.about-me').hide();
+})
+
+function change_colour(element) {
+    var list = ['#about-me', '#technical-skills'];
+    $(element).css('color', '#3EA6C1');
+    list.splice(list.indexOf(element), 1);
+    list.forEach(ele => {
+        $(ele).css('color', '#ffffff')
+    });
+}
